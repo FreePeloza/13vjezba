13ee7ca9f1d8847ca1bddecc4e911c90efc3d0e9
FreePeloza <stjepannovoselic1@gmail.com>
2017-05-15 09:37:49 +0200

    glavni izbornik

--- 13vjezba-Damijanic/zbroj.cpp
+++ 13vjezba-Damijanic/zbroj.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 int main()
 {
-    int a, b;
-    cin >> a >> b;
-    cout << a + b << endl;
+    cout<<"Glavni izbornik"<<endl;
+    cout<<"1.Unosnove osobe"<<endl;
+    cout<<"2.Ispsis nove podatke"<<endl;
+    cout<<"3.Pretraga prema racunu"<<endl;
+    cout<<"4.Pretraga prema prezimenu"<<endl;
+    cout<<"5.Ispisi sortirani"<<endl;
+    int izbor;
+    cin>>izbor;
 }
